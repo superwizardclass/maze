f7040dec94bb23087e1762b30d7e88198e909721
superwizardclass <wizardclassio@gmail.com>
2023-11-12 23:34:19 -0800

    Expand creating a maze function

--- maze_manager.py
+++ maze_manager.py
@@ -1,3 +1,16 @@
-class MazeManager():
-    def create_maze():
-        pass
+from maze import Maze
+from room import Room
+from door import Door
+
+
+class MazeManager:
+    def create_maze() -> Maze:
+        m1: Maze = Maze()
+        r1: Room = Room()
+        r2: Room = Room()
+        d1: Door = Door()
+
+        m1.add_room(r1)
+        m1.add_room(r2)
+
+        return m1
